0ca29e7af95cc171ea08d557b2d9b2fb5335a3aa
Pratik Dutta <pratikdutta.786@gmail.com>
2021-07-04 14:24:43 +0530

    Add Image from next, helps in responsiveness as well loads only if we scroll to the image reside

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,11 @@
 import Link from "next/link"
+import Image from "next/image"
 
 const Navbar = () => {
     return ( <nav>
         <div className="logo">
-    <h1>Ninja List</h1></div>
+            <Image src="/next_ninja.png" width={142} height={77}></Image>
+        </div>
     <Link href="/"><a>
         Home
     </a></Link>
