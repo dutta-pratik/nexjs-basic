7f927f9cd591a52902e13b305d01bdea1a4f9754
Pratik Dutta <pratikdutta.786@gmail.com>
2021-07-04 17:46:42 +0530

    dynamic routing

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import styles from '../../styles/Ninjas.module.css'
 
@@ -16,11 +17,11 @@ const NinjasList = ({ninjas}) => {
     return (<><h1>Ninjas List</h1>
     <p>Incididunt sit magna deserunt occaecat. Aute ea tempor ipsum veniam. Labore qui aliqua quis nulla do cupidatat excepteur pariatur laborum incididunt et. Eu duis amet proident laborum labore consectetur esse consectetur sint exercitation ut consequat consequat. Aliquip sint sint incididunt laborum mollit excepteur reprehenderit. Id ex consectetur in aliqua occaecat non dolor.</p>
     {ninjas.map(ninja => (
-        <div key={ninja.id}>
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ ninja.name }</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </>);
 }
