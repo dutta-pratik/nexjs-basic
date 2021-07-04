d90725bab881d90cacacfa44c8b364a769118a9d
Pratik Dutta <pratikdutta.786@gmail.com>
2021-07-04 14:28:00 +0530

    add metadata and title using Head component

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,13 @@
+import Head from "next/head";
+
 const About = () => {
-    return (<><h1>About</h1>
+    return (<>
+    
+    <Head>
+      <title>Ninja List | About</title>
+      <meta name="keyword" content="ninjas"></meta>
+    </Head>
+    <h1>About</h1>
     <p>Mollit sit qui incididunt dolor non in elit irure ipsum ut in sint reprehenderit proident. Voluptate et veniam culpa officia quis quis esse minim. Duis nisi dolore deserunt adipisicing magna excepteur dolor pariatur non est ut est. Adipisicing consequat tempor cupidatat minim aliquip cupidatat minim ea sit enim consectetur minim sunt. Id culpa id et elit exercitation ullamco. Anim tempor labore minim veniam ex elit pariatur aliqua anim pariatur consequat veniam in tempor. Amet cupidatat in laborum id nisi occaecat proident laboris dolor amet.</p>
     </>);
 }
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,11 @@ import Link from "next/link"
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Ninja List | Home</title>
+      <meta name="keyword" content="ninjas"></meta>
+    </Head>
     <div>
       
       <h1 className={styles.title}>Home</h1>
@@ -17,5 +22,6 @@ export default function Home() {
       </p>
       
     </div>
+    </>
   )
 }
